f725b1798b6f7d2651698c1fb5aed6fdfb437d46
Chindarat <buytaxusa@gmail.com>
2026-08-03 10:50:14 +0700

    add Students model and migration

--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+# st_id, fname, lname
+
+
+class Students(models.Model):
+
+    st_id = models.CharField(max_length=12, unique=True)
+    fname = models.CharField(max_length=100, blank=False)
+    lname = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.fname + " " + self.lname
+
+    def get_absolute_url(self):
+        return reverse("Student_detail", kwargs={"pk": self.pk})
+
