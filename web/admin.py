0d7e0f68270b2e0fe86341c1cc28fbb9120fb6e9
Chindarat <buytaxusa@gmail.com>
2026-08-03 10:59:35 +0700

    register Students model in admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Students
 
 # Register your models here.
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("st_id", "fname", "lname")
+
+
+admin.site.register(Students, StudentAdmin)
